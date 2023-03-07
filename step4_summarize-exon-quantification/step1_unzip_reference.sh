bdebdd30043790c0a2cd360bcb92f038cdeea3d2
Timothy Shaw <>
2023-03-07 18:02:51 -0500

    renamed step3 to step4

---
+++ step4_summarize-exon-quantification/step1_unzip_reference.sh
@@ -0,0 +1,3 @@
+
+gunzip gencode.v31.primary_assembly.exon.novelexon.txt.gz
+#curl --output gencode.v31.primary_assembly.exon.novelexon.txt  -kL https://www.dropbox.com/s/miq7zvv1qsfz973/gencode.v31.primary_assembly.exon.novelexon.txt?dl=0
