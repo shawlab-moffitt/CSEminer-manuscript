baf708715d597ae33055749a87985047ebaba60c
Timothy Shaw <>
2023-03-09 10:28:09 -0500

    Add step1 file

---
+++ 2_exon_prioritization/step2_perform_exon_prioritization/step1_copy_parameter_files.sh
@@ -0,0 +1,3 @@
+# execute the following script to copy the parameter files generated from step1
+cp ../step1_download_install_examples/CSI-Miner_BrainTumorOnly.param .
+cp ../step1_download_install_examples/CSI-Miner_SolidTumorOnly.param .
