138a81f625a2e2725dff397aef96400b2b6179b5
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2023-10-13 12:50:51 -0400

    Update step2_copy_bam_file_to_working_dir.sh

--- 1_rnaseq_mapping_exonquant/step2-exonquant-bam2htseq/step2_copy_bam_file_to_working_dir.sh
+++ 1_rnaseq_mapping_exonquant/step2-exonquant-bam2htseq/step2_copy_bam_file_to_working_dir.sh
@@ -1,2 +1,6 @@
-# copy the STAR mapped bam file to your working directory
-cp 
+# copy the STAR mapped bam file and index file to your current working directory
+# 
+# cp SJMEL001033_D1.STAR.Aligned.sortedByCoord.out.bam .
+# cp SJMEL001033_D1.STAR.Aligned.sortedByCoord.out.bam.bai .
+cp [bam file] [working directory]
+cp [bam index file] [working directory]
