dfa3917e6b9fc5ce1e8c16fcf90270f2d10f98d9
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2024-01-11 11:43:03 -0500

    Update step2_exon_prioritization_example.sh

--- 2_exon_prioritization/step4_predict_alternative_spliced_exons/step2_exon_prioritization_example.sh
+++ 2_exon_prioritization/step4_predict_alternative_spliced_exons/step2_exon_prioritization_example.sh
@@ -5,4 +5,7 @@ fi
 if test -f "gencode.v35.primary_assembly.annotation.gtf"; then
     curl --output gencode.v35.primary_assembly.annotation.gtf -kL https://www.dropbox.com/s/bc0fii0jsccwpie/gencode.v35.primary_assembly.annotation.gtf?dl=1
 fi
+
+# Custom script for prioritizing the exons
+# drppm -CSEminerPrioritizationScript [Comprehensive Exon Annotation] [GTF file] [Output AS Annoatation] [Output Exon Annotations]
 drppm -CSEminerPrioritizationScript Comprehensive_Exon_Annotations_20220217.txt gencode.v35.primary_assembly.annotation.gtf Output_Exon_AS_Annotations.txt Output_Exon_Annotations.txt
