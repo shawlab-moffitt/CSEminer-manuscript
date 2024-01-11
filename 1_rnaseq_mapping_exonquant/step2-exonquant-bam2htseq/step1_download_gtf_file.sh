f659c587606c433425ea3403453b890c30f9bbdd
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2024-01-11 10:21:48 -0500

    Update step1_download_gtf_file.sh

--- 1_rnaseq_mapping_exonquant/step2-exonquant-bam2htseq/step1_download_gtf_file.sh
+++ 1_rnaseq_mapping_exonquant/step2-exonquant-bam2htseq/step1_download_gtf_file.sh
@@ -1,1 +1,1 @@
-curl --output gencode.v31.primary_assembly.exon.novelexon.gtf -kL https://www.dropbox.com/s/visytv8td3ft7hk/gencode.v31.primary_assembly.exon.novelexon.gtf?dl=0
+curl --output gencode.v31.primary_assembly.exon.novelexon.gtf -kL https://www.dropbox.com/s/visytv8td3ft7hk/gencode.v31.primary_assembly.exon.novelexon.gtf?dl=1
