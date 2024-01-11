bd2dd8a101cf63a83e4c9ecf1ba0be99fc71118c
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2024-01-11 09:32:29 -0500

    Update step3_example_htseq_exon_quant.sh

--- 1_rnaseq_mapping_exonquant/step2-exonquant-bam2htseq/step3_example_htseq_exon_quant.sh
+++ 1_rnaseq_mapping_exonquant/step2-exonquant-bam2htseq/step3_example_htseq_exon_quant.sh
@@ -1,4 +1,4 @@
 # Execute the htseq quantification
-#htseq-count -f bam -a 0 -r pos -s no -m union -t exon --nonunique all SJMEL001033_D1.STAR.Aligned.sortedByCoord.out.bam /research/rgs01/project_space/zhanggrp/MethodDevelopment/common/ExonLevelQuantificationPipeline/Reference/ExonLevel_nosplit/gencode.v31.primary_assembly.exon.novelexon.gtf >SJMEL001033_D1_count.txt
+# example: htseq-count -f bam -a 0 -r pos -s no -m union -t exon --nonunique all SJMEL001033_D1.STAR.Aligned.sortedByCoord.out.bam /research/rgs01/project_space/zhanggrp/MethodDevelopment/common/ExonLevelQuantificationPipeline/Reference/ExonLevel_nosplit/gencode.v31.primary_assembly.exon.novelexon.gtf >SJMEL001033_D1_count.txt
 
 htseq-count -f bam -a 0 -r pos -s no -m union -t exon --nonunique all [bam file] [exon gtf file] > [output exon count file]
