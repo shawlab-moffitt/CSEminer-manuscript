c4f9bc7f269763f5643e997d259e41f0723af974
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2024-01-15 08:57:22 -0500

    Update step1_download_annotation_files.sh

--- 3_figure_data_generation/step1_download_annotation_files.sh
+++ 3_figure_data_generation/step1_download_annotation_files.sh
@@ -1,3 +1,3 @@
 # download the files from dropbox 
-curl --output pipeline_input_files.zip -kL https://www.dropbox.com/scl/fi/cem2argxukr5fqlb49m0k/pipeline_input_files.zip?rlkey=3wzegacn26m0sphz0bnsccu9b&dl=0
+curl --output pipeline_input_files.zip -kL https://www.dropbox.com/scl/fi/p9f1eemtc245iqlp6led0/pipeline_input_files.zip?rlkey=psy2gmvkuymd8nljdl8x05qid&dl=1
 
