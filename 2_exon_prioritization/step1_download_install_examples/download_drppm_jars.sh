d70809a1ca6378b2588a343179366259360d39ba
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2024-01-29 11:57:37 -0500

    Update download_drppm_jars.sh

--- 2_exon_prioritization/step1_download_install_examples/download_drppm_jars.sh
+++ 2_exon_prioritization/step1_download_install_examples/download_drppm_jars.sh
@@ -1,5 +1,5 @@
 
-curl --output DRPPM_20220511A_newmachine.jar -kL https://github.com/gatechatl/DRPPM/raw/master/export/DRPPM_20220511A_newmachine.jar
+curl --output DRPPM_20220511A_newmachine.jar -kL 'https://github.com/gatechatl/DRPPM/raw/master/export/DRPPM_20220511A_newmachine.jar'
 path2jar=`pwd`
 echo "#!/bin/bash\njava -Xmx64g -XX:-UseGCOverheadLimit -jar "$path2jar"/DRPPM_20220511A_newmachine.jar $@" > drppm
 chmod a+x drppm
