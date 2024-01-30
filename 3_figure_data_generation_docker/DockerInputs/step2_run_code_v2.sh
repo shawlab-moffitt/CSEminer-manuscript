0cb626cbfafb5f696ac7407de912eda4e53cb1fa
shawlab-moffitt <timothy.shaw@moffitt.org>
2024-01-30 17:40:13 -0500

    Update Docker Instance

---
+++ 3_figure_data_generation_docker/DockerInputs/step2_run_code_v2.sh
@@ -0,0 +1,4 @@
+
+# Categorize the exons for Figure 1.
+java -Xmx64g -XX:-UseGCOverheadLimit -jar DRPPM_20240112A_newmachine.jar -CSEminerFigure1ExonClassificationFullPipelineExecMode CompleteAnnotationPipeline/ OutputFolder
+
